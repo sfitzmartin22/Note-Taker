777285d69b096e0248da6e4a1ad57641449cb316
Sean Fitzmartin <sfitzmartin22@gmail.com>
2021-04-10 15:26:15 -0400

    setup the folder and file structure to complete the project

--- server.js
+++ server.js
@@ -11,4 +11,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
+
+// Starts the server to begin listening
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
